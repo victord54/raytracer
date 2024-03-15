d82c4537c241a8ef583c455cafd7e14896a06ac5
Victor Dallé <dallevictor@gmail.com>
2024-03-15 15:24:55 +0100

    simplify

--- src/image.hpp
+++ src/image.hpp
@@ -15,15 +15,45 @@ struct Image
 
     Image(int width, int height) : width(width), height(height), data(width * height) {}
 
-    void setPixel(int x, int y, Color color);
+    void setPixel(int x, int y, Color color)
+    {
+        if (x >= 0 && x < width && y >= 0 && y < height)
+        {
+            data[x + y * width] = color;
+        }
+    }
 
-    float getR(int x, int y) const;
+    float getR(int x, int y) const
+    {
+        return data[x + y * width].x();
+    }
 
-    float getG(int x, int y) const;
+    float getG(int x, int y) const
+    {
+        return data[x + y * width].y();
+    }
 
-    float getB(int x, int y) const;
+    float getB(int x, int y) const
+    {
+        return data[x + y * width].z();
+    }
 
-    void save(const std::string &filename) const;
+    void save(const std::string &filename) const
+    {
+        std::ofstream ofs(filename);
+        ofs << "P3\n"
+            << width << " " << height << "\n255\n";
+        for (int j = height - 1; j >= 0; j--)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                ofs << static_cast<int>(255.999 * data[i + j * width].x()) << ' '
+                    << static_cast<int>(255.999 * data[i + j * width].y()) << ' '
+                    << static_cast<int>(255.999 * data[i + j * width].z()) << '\n';
+            }
+        }
+        ofs.close();
+    }
 };
 
 #endif
